f8fcf29ae7b00f44b59b1db5d48e2131b2656be9
KIM JUNGHYUN <richg0ld@op.gg>
2019-08-10 23:40:03 +0900

    @types 아직 제대로 적용하기 어려워서 우선 제거

--- modules/loading.ts
+++ modules/loading.ts
@@ -1,4 +1,4 @@
-import { createAction, handleActions } from 'redux-actions';
+import { createAction, handleActions } from "redux-actions";
 
 export const START_LOADING = "START_LOADING";
 export const FINISH_LOADING = "FINISH_LOADING";
@@ -9,17 +9,17 @@ export const finishLoading = createAction(FINISH_LOADING, payload => payload);
 export const initialState = {};
 
 const reducer = handleActions(
-    {
-        [START_LOADING]: (state, action) => ({
-            ...state,
-            [action.payload]: true
-        }),
-        [FINISH_LOADING]: (state, action) => ({
-            ...state,
-            [action.payload]: false
-        })
-    },
-    initialState
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: true
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false
+    })
+  },
+  initialState
 );
 
 export default reducer;
