273a0b068bd48e2ff4bb2cca0c508c36464ad7b6
JUNGHYUN KIM <richg0ld@op.gg>
2019-08-15 23:27:49 +0900

    base layout 모두 적용

--- pages/path/[dynamic]/pizza.tsx
+++ pages/path/[dynamic]/pizza.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Base from "../../../components/Base";
 
 const Pizza = () => {
     const router = useRouter();
     return (
-        <div>
+        <Base>
             <h2>Pizza Page</h2>
             <h3>dynamic : {router.query.dynamic}</h3>
             { /*language=SCSS*/ }
             <style jsx>{`
           `}</style>
-        </div>
+        </Base>
     )
 };
 
--- pages/path/p/[param].tsx
+++ pages/path/p/[param].tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Base from "../../../components/Base";
 
 const Param = () => {
     const router = useRouter();
     return (
-        <div>
+        <Base>
             <h2>Param Page</h2>
             <h3>param : {router.query.param}</h3>
             { /*language=SCSS*/ }
             <style jsx>{`
           `}</style>
-        </div>
+        </Base>
     )
 };
 
